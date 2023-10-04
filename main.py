8b39c7620ea3627a48398c10ff1fcbc5984b1ce6
HarshithRajesh <harshithr2004@gmail.com>
2023-10-04 12:37:35 +0530

    update of student data

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ conn = psycopg2.connect(
 print("Connected to PostgreSQL!")
 
 def student_details():
+
     student_name = "Harshith"
     father_name = "Rajesh"
     mother_name = "Usha"
@@ -17,16 +18,14 @@ def student_details():
     phone1 = '3545'
     phone2 = '53153'
     cur = conn.cursor()
-    student_id = cur.execute("SELECT student_id FROM student_details ORDER BY student_id DESC LIMIT 1")
-    if student_id == None:
-        print(student_id)
-    else:
-        print('0')
 
-    # sql = '''INSERT INTO student_details
-    #         (student_id,student_name,father_name,mother_name,address,phone1,phone2)
-    #         VALUES (%s,%s,%s,%s,%s,%s,%s)'''
-    # cur.execute(sql,(student_id+1,student_name,father_name,mother_name,address,phone1,phone2))
+    sql = '''INSERT INTO student_details
+            (student_name,father_name,mother_name,address,phone1,phone2)
+            VALUES (%s,%s,%s,%s,%s,%s);'''
+    cur.execute(sql,(student_name,father_name,mother_name,address,phone1,phone2))
+    conn.commit()
     cur.close()
+    conn.close()
+
 
 student_details()
